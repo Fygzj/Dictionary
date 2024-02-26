6c5f008e0fb61db75689b90a865450e042a97c76
Fygzj <160886488+Fygzj@users.noreply.github.com>
2024-02-26 11:24:13 +0100

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,23 @@
+class Kon:
+
+    def __init__(self, race, age, speed):
+        self.race = race
+        self.age = age
+        self.speed = speed
+
+
+
+
+class Mysz:
+    def __init__(self, race, age, speed):
+        self.race = race
+        self.age = age
+        self.speed = speed
+
+
+class Zolwie:
+    def __init__(self, race, age, speed):
+        self.race = race
+        self.age = age
+        self.speed = speed
+
